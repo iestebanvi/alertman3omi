72102b12d2892e25488f553a6a5be152079f6c4a
iestebanvi <iestebanvi@gmail.com>
2021-08-04 09:49:46 +0200

    Primera Version

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.8
+ENV VAR1 IEV
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+WORKDIR /app
+COPY . /app
+RUN useradd appuser && chown -R appuser /app
+USER appuser
+
+EXPOSE 8080
+
+CMD [ "python3", "./mainalertman3omi.py"]
---
+++ mainalertman3omi.py
@@ -0,0 +1,27 @@
+from datetime import date, datetime
+from flask import Flask, request, jsonify, make_response, redirect, url_for
+import json
+
+app = Flask(__name__)
+
+@app.route('/status', methods=['GET'])
+def status():
+  return "I'm alive.", 200
+
+@app.route('/', methods=['GET'])
+def root():
+    return redirect(url_for('status'))
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+  alertFromAlertmanager = request.json
+  print("Incoming JSON:", alertFromAlertmanager)
+  reformatedRequestJson = json.dumps(alertFromAlertmanager) 
+  print("Incoming JSON Reformated", reformatedRequestJson)
+
+  return {'mensaje': 'recibido'}
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    
