d112863c7c5f8226eba06eb9422f2d79761711e4
Ismael Esteban <ismael.esteban@t-systems.com>
2021-08-04 16:41:14 +0200

    Main

--- mainAlertman2omi.py
+++ mainAlertman2omi.py
@@ -1,8 +1,21 @@
 from datetime import date, datetime
 from flask import Flask, request, jsonify, make_response, redirect, url_for
 import json
+import requests
+from xml.etree import ElementTree as et
 
 app = Flask(__name__)
+xmlTemplate="""
+<omi-event-message>
+<title></title>
+<description></description>
+<severity></severity>
+<node></node>
+<object></object>
+<category></category>
+<subcategory></subcategory>
+<affectedCI></affectedCI>
+</omi-event-message>"""
 
 @app.route('/status', methods=['GET'])
 def status():
@@ -13,6 +26,18 @@ def webhook():
   alertFromAlertmanager = request.json
   #print(json.dumps(alertFromAlertmanager))
   print("Incoming JSON: ", json.dumps(alertFromAlertmanager))
+
+# xml = """<?xml version='1.0' encoding='utf-8'?>
+# <a>б</a>"""
+# headers = {'Content-Type': 'application/xml'} # set what your server accepts
+# print requests.post('http://httpbin.org/post', data=xml, headers=headers).text
+
+  headers = {'Content-Type': 'application/xml'} # set what your server accepts
+  data=et.parse(xmlTemplate)
+  data.find('omi-event-message/title').text = 'Nueva Alarma'
+  data.write(xmlTemplate)
+  print ("data", data)
+  
   
   return {
     "mensaje": "recepcionado y devuelto",
