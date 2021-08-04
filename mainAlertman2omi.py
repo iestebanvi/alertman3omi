dedd4e3ba75385aad2a4c8d9a13036d70d31bf0d
Ismael Esteban <ismael.esteban@t-systems.com>
2021-08-04 13:49:26 +0200

    Push

--- mainAlertman2omi.py
+++ mainAlertman2omi.py
@@ -3,9 +3,7 @@ from flask import Flask, request, jsonify, make_response, redirect, url_for
 import json
 import gevent.pywsgi
 
-
 app = Flask(__name__)
-port = 8080
 
 @app.route('/status', methods=['GET'])
 def status():
@@ -23,7 +21,4 @@ def webhook():
     }
 
 if __name__ == '__main__':
-  #DEVEL
-  #app.run(host="0.0.0.0", port=8080)
-  app_server = gevent.pywsgi.WSGIServer((port), app)
-  app_server.serve_forever()
+  app.run(host="0.0.0.0", port=8080)
